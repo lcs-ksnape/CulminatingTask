823c532a3965974e0a388919c287b18074b2d038
Kat <ksnape@lcs.on.ca>
2021-04-28 13:07:08 -0400

    Wrote my name  out, letter by letter using the turtle metaphor

--- Playground.playground/Pages/Empty Canvas 1.xcplaygroundpage/Contents.swift
+++ Playground.playground/Pages/Empty Canvas 1.xcplaygroundpage/Contents.swift
@@ -53,7 +53,7 @@ turtle.currentHeading()
 
 // We want to draw the letter K
 turtle.forward(steps: 100)
-turtle.drawSelf()
+//turtle.drawSelf()
 
 // Turn the turtle around
 turtle.right(by: 180)
@@ -63,7 +63,79 @@ turtle.drawSelf()
 // Move the turtle back to middle of stick
 turtle.penUp()
 turtle.forward(steps: 50)
+//turtle.drawSelf()
+
+// Start drawing the first branch of the K
+turtle.left(by:135)
+//turtle.drawSelf()
+turtle.penDown()
+turtle.forward(steps: 85)
 turtle.drawSelf()
+
+// Drawing the second branch of the K
+turtle.penUp()
+turtle.backward(steps: 85)
+//turtle.drawSelf()
+turtle.right(by: 90)
+//turtle.drawSelf()
+turtle.penDown()
+turtle.forward(steps: 85)
+turtle.drawSelf()
+
+// Start Drawing the A
+turtle.penUp()
+turtle.left(by: 45)
+turtle.forward(steps: 25)
+turtle.left(by: 70)
+turtle.drawSelf()
+
+// First Leg of A
+turtle.forward(steps: 120)
+//turtle.drawSelf()
+
+// Drawing Second Leg
+turtle.right(by: 135)
+//turtle.drawSelf()
+turtle.forward(steps: 135)
+turtle.drawSelf()
+
+// Connecting Bridge
+turtle.backward(steps: 60)
+turtle.penUp()
+//turtle.drawSelf()
+turtle.right(by: 115)
+//turtle.drawSelf()
+turtle.penDown()
+turtle.forward(steps: 55)
+
+// Drawing the T
+turtle.penUp()
+turtle.right(by: 180)
+//turtle.drawSelf()
+turtle.forward(steps: 120)
+turtle.right(by: 90)
+turtle.forward(steps: 60)
+turtle.left(by: 180)
+//turtle.drawSelf()
+turtle.penDown()
+turtle.forward(steps: 120)
+//turtle.drawSelf()
+
+// Crossing off the T
+turtle.left(by: 90)
+turtle.penUp()
+turtle.forward(steps: 40)
+turtle.right(by: 180)
+turtle.penDown()
+turtle.forward(steps: 80)
+//turtle.drawSelf()
+
+
+
+
+
+
+
 /*:
  ## Show the Live View
  Don't see any results?
